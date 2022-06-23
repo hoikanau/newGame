8973b5a4a7770c615676b3e9c94ada8ce0632062
Jerry <hoikanau123@gmail.com>
2022-06-22 21:47:57 -0700

    6/22

---
+++ newGame/Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int y = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (y == 0)
+        {
+            Debug.Log("y is 0");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.)
+    }
+
+}
