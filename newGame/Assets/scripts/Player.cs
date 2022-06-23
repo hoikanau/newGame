934113c6110207f762602c95d70bdd007612a8d2
Jerry <hoikanau123@gmail.com>
2022-06-22 23:58:20 -0700

    6/22

--- newGame/Assets/scripts/Player.cs
+++ newGame/Assets/scripts/Player.cs
@@ -4,21 +4,64 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int y = 0;
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+    private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidbodyComponent;
+    private bool isGrounded;
+    private int superJumpsRemaining;
     
+
     // Start is called before the first frame update
     void Start()
     {
-        if (y == 0)
-        {
-            Debug.Log("y is 0");
-        }
+        rigidbodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.)
+        // Checking if player pressed space bar = Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyWasPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+
     }
+
+    // FixedUpdate is called once every physics update
+    private void FixedUpdate()
+    {
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, GetComponent<Rigidbody>().velocity.y , 0);
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            float jumpPower = 5f;
+            if (superJumpsRemaining > 0)
+            {
+                jumpPower *= 2;
+                superJumpsRemaining--;
+            }
+            rigidbodyComponent.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            Destroy(other.gameObject);
+            superJumpsRemaining++;
+        }
+    }
+
 
 }
